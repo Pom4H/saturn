610c6160c19c3c80b86180a89a2b579fba3e945f
Roman Popov <atfpom4h@gmail.com>
2026-09-22 00:39:59 +0300

    tooling: use one TypeScript compiler API

--- plant/connection-edit.ts
+++ plant/connection-edit.ts
@@ -1,4 +1,4 @@
-import ts from '@typescript/typescript6';
+import ts from 'typescript';
 import { compileProject } from './compiler';
 import { failCode } from './diagnostics';
 import type { Connection } from './ports';
