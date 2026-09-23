02822a725a7acb76e47638211321670533d70650
Roman Popov <atfpom4h@gmail.com>
2026-09-23 10:11:59 +0300

    refactor: use renderer scene registry

--- site/exports.ts
+++ site/exports.ts
@@ -1,4 +1,4 @@
-import type { Scene } from '../src/core';
+import type { Scene } from '../src/scene';
 export function downloadFile(name: string, data: string, type = 'text/plain;charset=utf-8') {
   const url = URL.createObjectURL(new Blob([data], { type }));
   const a = document.createElement('a'); a.href = url; a.download = name; a.click(); setTimeout(() => URL.revokeObjectURL(url), 1000);
